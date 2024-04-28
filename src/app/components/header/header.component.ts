9661e44d16443e48c8a05d7485e1e4d71755c289
Gabriel Castro <gabcas28@gmail.com>
2024-04-28 14:30:48 +0200

    feat(init): structure components and services

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './header.template.html',
+  styleUrl: './header.styles.css'
+})
+export class HeaderComponent {
+
+}
---
+++ src/app/views/favorites/favorites.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-store',
+  standalone: true,
+  imports: [],
+  templateUrl: './favorites.template.html',
+  styleUrl: './favorites.styles.css'
+})
+export class FavoritesComponent {
+
+}
